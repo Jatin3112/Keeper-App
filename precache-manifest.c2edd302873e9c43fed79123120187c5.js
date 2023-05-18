9cf770fed9c669c4bcb2f0b8870ed91b9bfa1d09
Tushya123 <tushyagandhi123@gmail.com>
2023-05-18 15:17:24 +0530

    Updates

---
+++ precache-manifest.c2edd302873e9c43fed79123120187c5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf5645ba70e570657279325a8f2e3fb",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/Keeper-App/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "6636a868178a5b30b322",
+    "url": "/Keeper-App/static/js/main.353da62e.chunk.js"
+  },
+  {
+    "revision": "4997685bab9cff726bba",
+    "url": "/Keeper-App/static/js/runtime-main.31b25eb3.js"
+  }
+]);
